0b274eb637f9f1f0e5563cca309f34404f8c7917
DarkSwarp <mihkleban@gmail.com>
2023-02-12 19:31:02 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+const body = document.querySelector("body");
+let color;
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+button.addEventListener("click", (event) => {
+    color = getRandomHexColor();
+    body.style.background = color;
+    textColor.textContent = color;
+});
