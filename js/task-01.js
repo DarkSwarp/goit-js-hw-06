15f1b4a085bd22c9bc595a7311b3fa2723bb7f3c
DarkSwarp <mihkleban@gmail.com>
2023-02-12 15:32:36 +0200

    task 2

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ console.log(`Number of categories: ${quantityH2El.length}`);
 
 const h2El = document.querySelectorAll(".item > h2");
 const ilEl = document.querySelectorAll(".item > ul");
-for (let i = 0; i < quantityH2El.length; i+=1) {
+for (let i = 0; i < quantityH2El.length; i += 1) {
     console.log(`Category: ${h2El[i].textContent}`);
     console.log(`Elements: ${ilEl[i].children.length}`);
-    }
+}
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+const ulEl = document.querySelector("#ingredients");
+let liEl;
+const arrayLiEl = [];
+
+ingredients.forEach((el) => {
+    liEl = document.createElement("li");
+    liEl.className = "item";
+    liEl.textContent = el;
+    arrayLiEl.push(liEl);
+});
+ulEl.append(...arrayLiEl);
