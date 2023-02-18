332957c2475001507ba10137d9bfacfc62b67142
DarkSwarp <mihkleban@gmail.com>
2023-02-18 19:59:06 +0200

    added task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const createButtonEl = document.querySelector("button[data-create]");
+const destroyButtonEl = document.querySelector("button[data-destroy]");
+const inputEl = document.querySelector("input");
+const boxesBox = document.querySelector("#boxes");
+let inputValue = 0;
+inputEl.addEventListener("input", (event) => {
+    inputValue = event.currentTarget.value;
+});
+createButtonEl.addEventListener("click", () => {
+    createBoxes(inputValue);
+});
+function createBoxes(inputValue) {
+    console.log(inputValue);
+    let startSize = 20;
+    let markup = "";
+    let color;
+    for (let i = 1; i <= inputValue; i += 1) {
+        color = getRandomHexColor();
+        startSize += 10;
+        markup += `<div style = "width: ${startSize}px; height: ${startSize}px; background: ${color}"></div>`;
+    }
+    boxesBox.insertAdjacentHTML("beforeend", markup);
+}
+destroyButtonEl.addEventListener("click", () => (boxesBox.innerHTML = ""));
